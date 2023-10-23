05727aa19b8a5bb6bbb3d41ee567e05efb014584
Dhanasekaran K P <dhanasekarancs@gmail.com>
2023-10-23 07:58:55 -0700

    Few cosmetic changes

--- TopKFrequent.cpp
+++ TopKFrequent.cpp
@@ -1,4 +1,6 @@
 // Code to return the top k frequent elements.
+// Min Heap
+// Time complexity is O(nlogk)
 
 #include <iostream>
 #include <algorithm>
@@ -20,10 +22,10 @@ void TopKValues(int *values, int n, int k)
 {
   Item *items;
   int index = 1;
-  
+
   sort(values, values + n);
   items = (Item *)malloc(n * sizeof(Item));
-  
+
   items[0].value = values[0];
   items[0].frequency = 1;
   for(int i = 1; i < n; ++i)
@@ -39,7 +41,7 @@ void TopKValues(int *values, int n, int k)
       index++;
     }
   }
-  
+
   sort(items, items + index, compare);
   for(int i = 0; i < k && i < index; ++i)
   {
@@ -51,15 +53,14 @@ int main()
 {
   int n, k;
   int *values;
-  
+
   cin >> n >> k;
   values = (int *)malloc(n * sizeof(int));
   for(int i = 0; i < n; ++i)
   {    
     cin >> values[i]; 
   }
-  
+
   TopKValues(values, n, k);
-  
   return 0;
 }
